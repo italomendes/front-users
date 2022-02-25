5b3e1aedd9fcafc047830eb652f573ad394eeebf
Italo Mendes <italomemendes@gmail.com>
2022-02-25 09:09:33 -0300

    feat: created auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,46 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 import { InfoService } from './info.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(
+    private infoService: InfoService,
+    private httpClient: HttpClient
+  ) {}
 
-  constructor(private infoService: InfoService, private httpClient: HttpClient) { }
-
-  public login(payload: {username: string, password: string}) {
+  public login(payload: { username: string; password: string }) {
     const endpoint = `${this.infoService.apiBase}/user/login`;
 
     return this.httpClient.post(endpoint, payload);
+  }
+
+  private setSession(token: string) {
+    const expiresAt = moment().add(3600, 'second');
+
+    localStorage.setItem('token', token);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expires_at');
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem('expires_at');
+    const expiresAt = JSON.parse(expiration!);
+    return moment(expiresAt);
   }
 }
